39aadbe1caf25c1429acd216360cc9adafe9ebba
Prosper Chidike Nwogu <generalpmoney@gmail.com>
2026-09-03 12:48:13 +0100

    Initial commit

---
+++ week01/Exercise1/Program.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello Welcome to CSE 210! the Home of Tech.");
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+        Console.WriteLine($"Nice to meet you, {lastName} {firstName}!");
+        Console.WriteLine("This is your first program in C#.");
+    }
+}
