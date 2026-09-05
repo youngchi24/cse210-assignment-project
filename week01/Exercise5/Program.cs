93f9afb71f24ce7cfba974c1e3911422196fbf29
Prosper Chidike Nwogu <generalpmoney@gmail.com>
2026-09-05 13:33:05 +0100

    finished all exercise work

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayMessage();
+        string name = UserName();
+        int number = UserNumber();
+        int Square = SquareNumber(number);
+        DisplayResult(name, Square);
+    }
+    static void DisplayMessage()
+    {
+        Console.WriteLine("Welcome to the program!.");
+    }
+
+    static string UserName()
+    {
+        Console.Write("Please enter your full name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+
+    static int UserNumber()
+    {
+        Console.Write("Enter a lucky number: ");
+        int number = int.Parse(Console.ReadLine());
+            return number;
+    }
+
+    static int SquareNumber(int number)
+    {
+        int square = number * number;
+        return square;
+    }
+
+    static void DisplayResult(string name, int square)
+    {
+        Console.WriteLine($"{name}, the square of your number is {square}");
     }
 }
